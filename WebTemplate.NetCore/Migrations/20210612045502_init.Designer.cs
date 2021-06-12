83d401f810b1a67fe86a6b70c9e2663d75f3fd18
Pablo B. Martinez Pedroso <pmartinezpedroso@gmail.com>
2021-06-11 22:57:49 -0600

    Change SQL server by Sqlite as database provider

--- WebTemplate.NetCore/Migrations/20210612045502_init.Designer.cs
+++ WebTemplate.NetCore/Migrations/20210612045502_init.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Infrastructure.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace WebTemplate.NetCore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210612045502_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebTemplate.NetCore/Migrations/20210612045502_init.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebTemplate.NetCore.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Conexiones",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Servidor = table.Column<string>(type: "TEXT", nullable: false),
+                    Puerto = table.Column<int>(type: "INTEGER", nullable: false),
+                    BaseDatos = table.Column<string>(type: "TEXT", nullable: false),
+                    Path = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conexiones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Salvas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ConexionId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Salvas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Salvas_Conexiones_ConexionId",
+                        column: x => x.ConexionId,
+                        principalTable: "Conexiones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Salvas_ConexionId",
+                table: "Salvas",
+                column: "ConexionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Salvas");
+
+            migrationBuilder.DropTable(
+                name: "Conexiones");
+        }
+    }
+}
